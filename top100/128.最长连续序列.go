8f70d9eef29f8ea33d133d504179256810ff3180
mingzaily <mingzaily@163.com>
2023-10-17 21:04:19 +0800

    top100: 哈希

--- top100/128.最长连续序列.go
+++ top100/128.最长连续序列.go
@@ -6,7 +6,29 @@
 
 // @lc code=start
 func longestConsecutive(nums []int) int {
-
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return 1
+	}
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+	maxLen := 1
+	currentLen := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1]+1 {
+			currentLen++
+		} else if nums[i] != nums[i-1] {
+			currentLen = 1
+		}
+		if currentLen > maxLen {
+			maxLen = currentLen
+		}
+	}
+	return maxLen
 }
+
 // @lc code=end
 
